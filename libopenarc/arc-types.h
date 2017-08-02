e09909f45901b486d4cfb5cff391c58424aacf87
Gene Shuman <gene@valimail.com>
2017-08-02 19:12:52 +0000

    Split signing and verification to enable SHA-1 handling

--- libopenarc/arc-types.h
+++ libopenarc/arc-types.h
@@ -179,8 +179,10 @@ struct arc_msghandle
 	struct arc_dstring *	arc_hdrbuf;
 	struct arc_canon *	arc_sealcanon;
 	struct arc_canon **	arc_sealcanons;
-	struct arc_canon *	arc_hdrcanon;
-	struct arc_canon *	arc_bodycanon;
+	struct arc_canon *	arc_valid_hdrcanon;
+	struct arc_canon *	arc_sign_hdrcanon;
+	struct arc_canon *	arc_valid_bodycanon;
+	struct arc_canon *	arc_sign_bodycanon;
 	struct arc_canon *	arc_canonhead;
 	struct arc_canon *	arc_canontail;
 	struct arc_hdrfield *	arc_hhead;
