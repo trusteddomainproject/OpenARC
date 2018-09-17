6ac7e12d753b9c469b07e64910f5a52927870840
Murray S. Kucherawy <msk@trusteddomain.org>
2018-09-17 11:54:21 -0700

    Fix AAR generation.

--- libopenarc/arc.c
+++ libopenarc/arc.c
@@ -3205,7 +3205,7 @@ arc_getseal(ARC_MESSAGE *msg, ARC_HDRFIELD **seal, char *authservid,
 	**  Part 1: Construct a new AAR
 	*/
 
-	arc_dstring_printf(dstr, "ARC-Authentication-Results:i=%u; %s; %s",
+	arc_dstring_printf(dstr, "ARC-Authentication-Results:i=%u; %s; arc=%s",
 	                   msg->arc_nsets + 1,
 	                   msg->arc_authservid,
 	                   ar == NULL ? "none" : (char *) ar);
