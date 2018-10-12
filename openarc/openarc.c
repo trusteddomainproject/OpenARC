bf9d75e04f034a907849eab3c88081086e70e753
Murray S. Kucherawy <msk@trusteddomain.org>
2018-10-12 00:02:07 -0700

    Remove redundant test

--- openarc/openarc.c
+++ openarc/openarc.c
@@ -3518,8 +3518,6 @@ mlfi_eom(SMFICTX *ctx)
 				for (n = 0; n < ar.ares_count; n++)
 				{
 					if (ar.ares_result[n].result_method == ARES_METHOD_ARC &&
-					    BITSET(ARC_MODE_SIGN,
-					           cc->cctx_mode) &&
 					    !BITSET(ARC_MODE_VERIFY,
 					            cc->cctx_mode))
 					{
