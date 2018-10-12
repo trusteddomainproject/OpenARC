a0d3885c6c9a630f3754564632560b051580b242
Murray S. Kucherawy <msk@trusteddomain.org>
2018-10-11 23:56:22 -0700

    Take a run at fixing AAR syntax

--- libopenarc/arc.c
+++ libopenarc/arc.c
@@ -3206,10 +3206,10 @@ arc_getseal(ARC_MESSAGE *msg, ARC_HDRFIELD **seal, char *authservid,
 	**  Part 1: Construct a new AAR
 	*/
 
-	arc_dstring_printf(dstr, "ARC-Authentication-Results:i=%u; %s; arc=%s",
+	arc_dstring_printf(dstr, "ARC-Authentication-Results:i=%u; %s; %s",
 	                   msg->arc_nsets + 1,
 	                   msg->arc_authservid,
-	                   ar == NULL ? "none" : (char *) ar);
+	                   ar == NULL ? "arc=none" : (char *) ar);
 	status = arc_parse_header_field(msg, arc_dstring_get(dstr),
 	                                arc_dstring_len(dstr), &h);
 	if (status != ARC_STAT_OK)
